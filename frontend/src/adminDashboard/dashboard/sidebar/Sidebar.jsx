52b84853903daf873d4c4dfe152837dad12dd4c1
gitbimashthapa <mailbimashthapa@gmail.com>
2025-08-23 09:44:50 +0545

    your commit message

--- frontend/src/adminDashboard/dashboard/sidebar/Sidebar.jsx
+++ frontend/src/adminDashboard/dashboard/sidebar/Sidebar.jsx
@@ -7,8 +7,8 @@ import {
   FiShoppingCart,
   FiImage, 
   FiHome, 
-  FiLogOut,FiBarChart,
-  FiLayers, FiSettings 
+  FiLogOut,
+  FiLayers 
 } from "react-icons/fi";
 
 const Sidebar = () => {
@@ -19,18 +19,9 @@ const Sidebar = () => {
   };
   return (
     <div className="w-72 bg-white shadow-xl border-r border-gray-200 min-h-screen p-6 flex flex-col text-gray-800 text-base sm:w-64 md:w-72">
-      <div className="flex items-center space-x-4 mb-12">
-        <Link to="/adminProfile">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuXKMwBGR8gRhpZlFDGIhWEbxiKs8sHUrpcg&s"
-            alt="Admin"
-            className="w-14 h-14 rounded-full object-cover shadow-md"
-          />
-        </Link>
-        <div>
-          <h3 className="text-lg font-semibold">Admin</h3>
-          <p className="text-sm text-green-500 mt-1">Online</p>
-        </div>
+      <div className="mb-12">
+        <h2 className="text-2xl font-bold text-gray-800">Admin Dashboard</h2>
+        <p className="text-sm text-green-500 mt-1">Online</p>
       </div>
 
       <nav className="flex-1 space-y-2">
@@ -44,8 +35,6 @@ const Sidebar = () => {
        
         <SidebarLink to="/addCategory" icon={<FiPlusCircle />} label="Add Category" />
         <SidebarLink to="/categoryList" icon={<FiLayers />} label="All Categories" />
-
-        <SidebarLink to="/settings" icon={<FiSettings />} label="Settings" />
 
 
         <div onClick={handleLogout} className="flex items-center px-4 py-3 rounded-lg hover:bg-red-50 text-red-600 transition duration-150 mt-4 cursor-pointer">
