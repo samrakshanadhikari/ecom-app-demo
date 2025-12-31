69defcac2862f1983b0b1075ae5d2734f9de1c2d
Samrakshyan Adhikari <samrakshanadhikari@users.noreply.github.com>
2025-12-30 23:31:23 -0600

    Fix: Remove default Content-Type header to allow FormData to work correctly

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -13,8 +13,10 @@ const API=axios.create({
 const APIAuthenticated=axios.create({
     baseURL : 'https://ecom-app-demo-backend.onrender.com',
     headers :{
-        'Content-Type' : 'application/json',
         'Accept' : 'application/json'
+        // Note: Content-Type is NOT set here - it will be set per request
+        // For FormData, browser will set it automatically
+        // For JSON, interceptor will set it
     }
 })
 
@@ -23,7 +25,7 @@ APIAuthenticated.interceptors.request.use((config)=>{
     if(token){
         config.headers.Authorization=token;
     }
-    // For FormData, remove Content-Type header so browser can set it with boundary
+    // For FormData, ensure Content-Type is NOT set (browser will set it with boundary)
     if (config.data instanceof FormData) {
         delete config.headers['Content-Type'];
         delete config.headers['content-type'];
@@ -31,6 +33,11 @@ APIAuthenticated.interceptors.request.use((config)=>{
         console.log("  - URL:", config.url);
         console.log("  - Method:", config.method);
         console.log("  - Has token:", !!token);
+    } else {
+        // For non-FormData requests, set Content-Type to application/json
+        if (!config.headers['Content-Type']) {
+            config.headers['Content-Type'] = 'application/json';
+        }
     }
     return config;
 }, (error)=> Promise.reject(error)
